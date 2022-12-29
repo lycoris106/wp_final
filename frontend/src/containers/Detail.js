b804dadce18f2266da33754f139dcc80cc992c30
chih-fu-Lai <jeffatwow@gmail.com>
2022-12-29 19:29:00 +0800

    from laptop

--- frontend/src/containers/Detail.js
+++ frontend/src/containers/Detail.js
@@ -1,16 +1,61 @@
 import { useContext, useEffect } from "react";
 import Layout from "../components/Layout/Layout.js";
 import Instruction from "../components/Instruction.js";
-import { Box, Container, Typography, CardMedia } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import {
+  Box,
+  Container,
+  Typography,
+  CardMedia,
+  Card,
+  Grid,
+  Paper,
+  Divider,
+  List,
+  ListItem
+} from "@mui/material";
+
+const StyledPaper = styled(Paper)(({theme, ...props}) => {
+  console.log(props);
+  return {
+    backgroundColor: '#f5f5f5',
+    padding: 10
+  };
+});
 
 const Detail = () => {
+
   const title = "Chocolate"
   const imgURL = require("../devFiles/img/4405dc8988.jpg");
   const instructions = ["Melt the chocolate", "..."];
+
   return (
     <Layout>
-      { /* todo */ }
-      <Container  sx={{ display: "flex", flexDirection: "column", rowGap: "10px" }} >
+      <Grid container spacing={2}>
+        <Grid xs={9}>
+          <Card sx={{ width: 700, height:650 }}>
+            <Typography gutterBottom variant="h3" component="div" align="center" sx={{ fontWeight: 'bold' }}>
+              {title}
+            </Typography>
+            <CardMedia
+              component="img"
+              image= {imgURL}
+            />
+          </Card>
+        </Grid>
+        <Grid xs={3}>
+          <StyledPaper elevation={2} >
+            <Typography variant="h5" gutterBottom>
+              Ingredient List
+            </Typography>
+            <Divider />
+            <List>
+              <ListItem>item</ListItem>
+            </List>
+          </StyledPaper>
+        </Grid>
+      </Grid>
+      {/* <Container  sx={{ display: "flex", flexDirection: "column", rowGap: "10px" }} >
       <Container maxWidth="md" sx={{
         display: 'flex',
         justifyContent: "space-between",
@@ -33,7 +78,7 @@ const Detail = () => {
           <Instruction instruction={ins} index={index} />
         ))}
       </Container>
-      </Container>
+      </Container> */}
     </Layout>
   );
 }
