5a10c2836f75bfc65dd5b3019b8b7772aa6afc06
victorkuoz <b08902069@csie.ntu.edu.tw>
2022-12-31 20:26:54 +0800

    add mutation gql

---
+++ frontend/src/graphql/mutations.js
@@ -0,0 +1,51 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_RECIPE_MUTATION = gql`
+    mutation CreateRecipe($input: CreateRecipeInput!) {
+        createRecipe(input: $input) {
+            id
+            title
+            content
+            image_url
+            ingredients
+            instructions {
+                title
+                contents
+            }
+            tags
+            time {
+                preparation
+                cook
+                cleanup
+            }
+        }
+    }
+`;
+
+export const UPDATE_RECIPE_MUTATION = gql`
+    mutation UpdateRecipe($input: UpdateRecipeInput!) {
+        updateRecipe(input: $input) {
+            id
+            title
+            content
+            image_url
+            ingredients
+            instructions {
+                title
+                contents
+            }
+            tags
+            time {
+                preparation
+                cook
+                cleanup
+            }
+        }
+    }
+`;
+
+export const DELETE_RECIPE_MUTATION = qgl`
+    mutation DeleteRecipe($id: ID!) {
+        deleteRecipe(id: $id)
+    }
+`;
--- frontend/src/graphql/query.js
+++ frontend/src/graphql/query.js
@@ -1,13 +1,35 @@
 import { gql } from '@apollo/client';
 
 export const SEARCH_QUERY = gql`
-  query recipes($ingredients: [String]!) {
-    recipes(ingredients: $ingredients) {
-      id
-      title
-      image_url
-      ingredients
-      tags
+    query recipes($ingredients: [String]!) {
+        recipes(ingredients: $ingredients) {
+            id
+            title
+            image_url
+            ingredients
+            tags
+        }
     }
-  }
 `;
+
+export const GET_RECIPE_QUERY = gql`
+    query recipe($id: ID!) {
+        recipe(id: $id) {
+            id
+            title
+            content
+            image_url
+            ingredients
+            instructions {
+                title
+                contents
+            }
+            tags
+            time {
+                preparation
+                cook
+                cleanup
+            }
+        }
+    }
+`
