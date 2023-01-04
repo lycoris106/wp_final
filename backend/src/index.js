88e0d0faf99a912491673223c5d4b5a9ff8fe111
致甫 賴 <b08902024@ntu.edu.tw>
2023-01-04 23:50:01 +0800

    fix index js bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import httpServer from "./server.js";
 import "dotenv-defaults/config.js";
 
 mongo.connect();
-const port = process.env.PORT | 4000;
+const port = process.env.PORT || 4000;
 
 httpServer.listen({ port }, () => {
     console.log(`The server is up on port ${port}!`);
